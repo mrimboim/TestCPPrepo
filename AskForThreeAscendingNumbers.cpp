6a39771e177b93507810b4d912b0a39969f4d0a2
Michael Rimboim <mrimboim@gmail.com>
2021-04-26 23:58:13 -0700

    done with project pretty much will submit now

--- AskForThreeAscendingNumbers.cpp
+++ AskForThreeAscendingNumbers.cpp
@@ -1,14 +1,40 @@
 #include <iostream>
-#include <vector>
-#include <string>
 
 //using nampspace std; (then you dont need to use std:: not proper ? *)
 
 int main()
 {
-    int numberOne = 0;
-    std::cout << "Please Enter 3 Numbers in ascending order\n";
+    //getting the intial number 
+    int numberOne;
+    std::cout << "Please enter 3 numbers in ascending order.\n";
+    std::cout << "Please enter 1st number: ";
     std::cin >> numberOne;
+
+    //getting the second number then running while loop till parameters met
+    int numberTwo;
+    std::cout << "Please enter 2nd number: ";
+    std::cin >> numberTwo;
+    while (numberOne >= numberTwo) {
+        std::cout << "It is smaller than the first number. Please try again.\n";
+        std::cout << "Please enter 2nd number: ";
+        std::cin >> numberTwo;
+
+    }
+    //getting third number 
+    int numberThree;
+    std::cout << "Please enter 3rd number: ";
+    std::cin >> numberThree;
+    while (numberTwo >= numberThree) {
+        std::cout << "It is smaller than the second number. Please try again.\n";
+        std::cout << "Please enter 3nd number: ";
+        std::cin >> numberThree;
+
+    }
+
+    //outputting all three with delimiters and text 
+    std::cout <<"The 3 ascending numbers are: " << numberOne << " - " << numberTwo << " - " << numberThree;
+
+    
     
     
     
--- README.txt
+++ README.txt
@@ -1,3 +1,8 @@
-Made by Michael Rimboim 
-Test read me file to practice documenting the code 
-letss goo 
+Made by Michael Rimboim 4/26/2021 Student Id:918533698
+
+To check the code please open the .cpp file 
+
+To run I would reccomend running through cmd the .exe file 
+
+Description:
+    This program takes 3 user inputs, which are supposed to be integers and in ascending order and spits them out in ascending order 
